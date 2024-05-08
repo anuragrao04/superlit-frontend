dc569fa64ac91c881faabbbe43ddebd5f0db819c
anuragrao04 <raoanu2004@gmail.com>
2024-05-08 14:45:47 +0530

    feat: monaco editor works offline now

--- src/routes/test/test.tsx
+++ src/routes/test/test.tsx
@@ -1,18 +1,28 @@
-import Editor from "@monaco-editor/react";
+import Editor, { loader } from "@monaco-editor/react";
+import * as monaco from "monaco-editor";
+
 import { useParams } from "react-router-dom";
+
+loader.config({
+  monaco,
+});
+
+loader.init().then((editorInstance) => {
+  console.log("Editor Loaded!");
+  console.log(editorInstance);
+});
 
 export default function Test() {
   let { testID } = useParams();
+
   return (
     <div className="bg-black h-screen">
-      <div className="text-2xl text-center font-mono p-10 text-white">
-        <Editor
-          height="90vh"
-          defaultLanguage="c"
-          defaultValue={"// test: " + testID}
-          theme="vs-dark"
-        />
-      </div>
+      <Editor
+        height="100vh"
+        defaultLanguage="c"
+        defaultValue={"// test: " + testID}
+        theme="vs-dark"
+      />
     </div>
   );
 }
