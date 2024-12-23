e6d049e84aa804e2c1811a07abdafa2e6a51f5a7
anuragrao04 <raoanu2004@gmail.com>
2024-12-23 13:19:52 +0530

    fix: vulnerability where teacher can login as student and vice versa

--- src/routes/homePages/student/studentHomePage.tsx
+++ src/routes/homePages/student/studentHomePage.tsx
@@ -1,54 +1,62 @@
-import { Boxes } from "@/components/ui/background-boxes"
-import { useLocation, useNavigate } from "react-router-dom"
-import { useEffect, useRef, useState } from "react"
-import SuperlitLogo from "@/components/superlitLogo"
-import { Button } from "@/components/ui/button"
-import ClassroomCard, { JoinClassroomCard } from "./../components/classroomCard"
-import { useAuth } from "@/lib/authContext"
-import AlertDialogWrapper from "@/components/ui/alertDialogWrapper"
-import BottomBar from "@/components/bottomBar.tsx"
-
+import { Boxes } from "@/components/ui/background-boxes";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import SuperlitLogo from "@/components/superlitLogo";
+import { Button } from "@/components/ui/button";
+import ClassroomCard, {
+  JoinClassroomCard,
+} from "./../components/classroomCard";
+import { useAuth } from "@/lib/authContext";
+import AlertDialogWrapper from "@/components/ui/alertDialogWrapper";
+import BottomBar from "@/components/bottomBar.tsx";
 
 export default function StudentHomePage() {
-  const { state } = useLocation()
-  const navigate = useNavigate()
-  const [userData, setUserData] = useState(null)
-  const { token, logout } = useAuth()
-  const dialogRef = useRef(null)
+  const { state } = useLocation();
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState(null);
+  const { token, logout } = useAuth();
+  const dialogRef = useRef(null);
   const [dialog, setDialog] = useState({
     title: "",
-    description: ""
-  })
+    description: "",
+  });
 
   function fetchUserData() {
     fetch("/api/auth/getuser", {
       headers: {
-        'Authorization': token.toString()
-      }
-    }
-    ).then((response: any) => {
-      if (response.status == 401) {
-        logout()
-        navigate("/")
-        return
-      }
-      return response.json()
-    }).then((data: any) => {
-      setUserData(data)
+        Authorization: token.toString(),
+      },
     })
-
+      .then((response: any) => {
+        if (response.status == 401) {
+          logout();
+          navigate("/");
+          return;
+        }
+        return response.json();
+      })
+      .then((data: any) => {
+        setUserData(data);
+        if (data.isTeacher) {
+          navigate("/home/teacher");
+        }
+      });
   }
-
 
   useEffect(() => {
     if (token == null) {
-      navigate("/")
-      return
+      navigate("/");
+      return;
     }
-    fetchUserData()
+    fetchUserData();
   }, []);
 
-  if (userData == null) return <div className="flex h-screen w-screen justify-center items-center">loading...</div>
+  if (userData == null)
+    return (
+      <div className="flex h-screen w-screen justify-center items-center">
+        loading...
+      </div>
+    );
 
   return (
     <div className="h-screen w-screen overflow-hidden">
@@ -56,39 +64,56 @@ export default function StudentHomePage() {
         <SuperlitLogo />
         <div className="flex items-center justify-center space-x-2 mr-5">
           <div>{userData.name + " [" + userData.universityID + "]"}</div>
-          <Button variant="outline" onClick={() => {
-            logout()
-            navigate("/")
-          }}>Logout</Button>
+          <Button
+            variant="outline"
+            onClick={() => {
+              logout();
+              navigate("/");
+            }}
+          >
+            Logout
+          </Button>
         </div>
       </div>
 
-
-
       <div className="h-[92vh] flex justify-center items-center bg-gray-100 dark:bg-gray-900">
-        <div className="grid" style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(3, minmax(0, 1fr))",
-          gridGap: "28px"
-        }}>
+        <div
+          className="grid"
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(3, minmax(0, 1fr))",
+            gridGap: "28px",
+          }}
+        >
+          {userData == null
+            ? "loading..."
+            : userData.classrooms.map((classroom: any, index: number) => (
+                <div
+                  key={index}
+                  onClick={() =>
+                    navigate("/home/student/classroom/" + classroom.code)
+                  }
+                >
+                  <ClassroomCard
+                    name={classroom.name}
+                    code={classroom.code}
+                    teacherCode=""
+                  />
+                </div>
+              ))}
 
-          {userData == null ? 'loading...' : userData.classrooms.map((classroom: any, index: number) => (
-            <div key={index} onClick={() => navigate("/home/student/classroom/" + classroom.code)}>
-              <ClassroomCard name={classroom.name} code={classroom.code} teacherCode="" />
-            </div>
-          ))
-          }
-
-          <JoinClassroomCard token={token} setDialog={setDialog} dialogRef={dialogRef} fetchUserData={fetchUserData} />
-
+          <JoinClassroomCard
+            token={token}
+            setDialog={setDialog}
+            dialogRef={dialogRef}
+            fetchUserData={fetchUserData}
+          />
         </div>
       </div>
 
       <BottomBar />
 
       <AlertDialogWrapper dialog={dialog} dialogRef={dialogRef} />
-
-    </div >
-  )
+    </div>
+  );
 }
-
--- src/routes/homePages/teacher/teacherHomePage.tsx
+++ src/routes/homePages/teacher/teacherHomePage.tsx
@@ -1,51 +1,61 @@
-import { useLocation, useNavigate } from "react-router-dom"
-import { useEffect, useRef, useState } from "react"
-import SuperlitLogo from "@/components/superlitLogo"
-import { Button } from "@/components/ui/button"
-import ClassroomCard, { CreateClassroomCard, JoinClassroomCard } from "./../components/classroomCard"
-import { useAuth } from "@/lib/authContext"
-import AlertDialogWrapper from "@/components/ui/alertDialogWrapper"
-import BottomBar from "@/components/bottomBar.tsx"
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import SuperlitLogo from "@/components/superlitLogo";
+import { Button } from "@/components/ui/button";
+import ClassroomCard, {
+  CreateClassroomCard,
+  JoinClassroomCard,
+} from "./../components/classroomCard";
+import { useAuth } from "@/lib/authContext";
+import AlertDialogWrapper from "@/components/ui/alertDialogWrapper";
+import BottomBar from "@/components/bottomBar.tsx";
 
 export default function TeacherHomePage() {
-  const navigate = useNavigate()
-  const [userData, setUserData] = useState(null)
-  const { token, logout } = useAuth()
-  const dialogRef = useRef(null)
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState(null);
+  const { token, logout } = useAuth();
+  const dialogRef = useRef(null);
   const [dialog, setDialog] = useState({
     title: "",
-    description: ""
-  })
+    description: "",
+  });
 
   function fetchUserData() {
     fetch("/api/auth/getuser", {
       headers: {
-        'Authorization': token.toString()
-      }
-    }
-    ).then((response: any) => {
-      if (response.status == 401) {
-        logout()
-        navigate("/")
-        return
-      }
-      return response.json()
-    }).then((data: any) => {
-      setUserData(data)
+        Authorization: token.toString(),
+      },
     })
-
+      .then((response: any) => {
+        if (response.status == 401) {
+          logout();
+          navigate("/");
+          return;
+        }
+        return response.json();
+      })
+      .then((data: any) => {
+        setUserData(data);
+        if (!data.isTeacher) {
+          navigate("/home/student");
+        }
+      });
   }
-
 
   useEffect(() => {
     if (token == null) {
-      navigate("/")
-      return
+      navigate("/");
+      return;
     }
-    fetchUserData()
+    fetchUserData();
   }, []);
 
-  if (userData == null) return <div className="flex h-screen w-screen justify-center items-center">loading...</div>
+  if (userData == null)
+    return (
+      <div className="flex h-screen w-screen justify-center items-center">
+        loading...
+      </div>
+    );
 
   return (
     <div className="h-screen w-screen overflow-hidden">
@@ -53,51 +63,79 @@ export default function TeacherHomePage() {
         <SuperlitLogo />
         <div className="flex items-center justify-center space-x-2 mr-5">
           <div>{userData.name + " [" + userData.universityID + "]"}</div>
-          <Button variant="outline" onClick={() => {
-            logout()
-            navigate("/")
-          }}>Logout</Button>
+          <Button
+            variant="outline"
+            onClick={() => {
+              logout();
+              navigate("/");
+            }}
+          >
+            Logout
+          </Button>
         </div>
       </div>
 
-
-
       <div className="h-[92vh] flex flex-col justify-between items-center p-5 bg-gray-100 dark:bg-gray-900">
-        <div className="grid" style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(3, minmax(0, 1fr))",
-          gridGap: "28px"
-        }}>
+        <div
+          className="grid"
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(3, minmax(0, 1fr))",
+            gridGap: "28px",
+          }}
+        >
+          {userData == null
+            ? "loading..."
+            : userData.classrooms.map((classroom: any, index: number) => (
+                <div
+                  onClick={() =>
+                    navigate("/home/teacher/classroom/" + classroom.code, {
+                      state: {
+                        classrooms: userData.classrooms,
+                      },
+                    })
+                  }
+                  key={index}
+                >
+                  <ClassroomCard
+                    name={classroom.name}
+                    code={classroom.code}
+                    teacherCode={classroom.teacherCode}
+                  />
+                </div>
+              ))}
 
-          {userData == null ? 'loading...' : userData.classrooms.map((classroom: any, index: number) => (
-            <div onClick={() => navigate("/home/teacher/classroom/" + classroom.code, {
-              state: {
-                classrooms: userData.classrooms
-              }
-            })} key={index}>
-              <ClassroomCard name={classroom.name} code={classroom.code} teacherCode={classroom.teacherCode} />
-            </div>
-          ))
-          }
-
-          <JoinClassroomCard token={token} setDialog={setDialog} dialogRef={dialogRef} fetchUserData={fetchUserData} />
-          <CreateClassroomCard token={token} setDialog={setDialog} dialogRef={dialogRef} fetchUserData={fetchUserData} />
+          <JoinClassroomCard
+            token={token}
+            setDialog={setDialog}
+            dialogRef={dialogRef}
+            fetchUserData={fetchUserData}
+          />
+          <CreateClassroomCard
+            token={token}
+            setDialog={setDialog}
+            dialogRef={dialogRef}
+            fetchUserData={fetchUserData}
+          />
         </div>
 
         <div className="w-full flex justify-end items-center">
-          <Button className="text-xl" onClick={() => navigate(`/home/teacher/classroom/assignment/newassignment`, {
-            state: {
-              classrooms: userData.classrooms
+          <Button
+            className="text-xl"
+            onClick={() =>
+              navigate(`/home/teacher/classroom/assignment/newassignment`, {
+                state: {
+                  classrooms: userData.classrooms,
+                },
+              })
             }
-          })}>
+          >
             + New Assignment
           </Button>
-
         </div>
       </div>
       <BottomBar />
       <AlertDialogWrapper dialog={dialog} dialogRef={dialogRef} />
-
-    </div >
-  )
+    </div>
+  );
 }
